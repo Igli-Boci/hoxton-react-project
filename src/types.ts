94e05a67a398b969804602c749b9a5d2f677ef9e
Igli-Boci <igliboci10@gmai.com>
2022-08-26 02:56:56 +0200

    Adding basket in the database and type file

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,15 @@
 export type Product = {
-  id: String;
+  id: number;
   title: string;
   price: number;
   image: string;
   category: string;
   desc: string;
+};
+
+export type BasketItem = {
+  id: number;
+  productId: number;
+  quantity: number;
+  product: Product;
 };
