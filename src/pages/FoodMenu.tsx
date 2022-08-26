ee3ad9d01efb0808ccb325f8a70c665d25243022
Igli-Boci <igliboci10@gmai.com>
2022-08-26 02:55:59 +0200

    Fetching menu from the server

--- src/pages/FoodMenu.tsx
+++ src/pages/FoodMenu.tsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Product } from "../types";
+import { Container, Row, Col } from "reactstrap";
+
 function FoodMenu() {
-  return <div>Menu</div>;
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/products")
+      .then((resp) => resp.json())
+      .then((productsFromServer) => setProducts(productsFromServer));
+  }, []);
+  return (
+    <section className="products-container main-wrapper">
+      <Container>
+        <Row>
+          <Col>
+            {products.map((product) => (
+              <Link to={`/menu/${product.id}`}>
+                <img src={product.image} alt={product.title} />
+                <h5>{product.title}</h5>
+              </Link>
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
+
 export default FoodMenu;
